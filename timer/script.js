1e372793ce6013710c1f65a91ed20e11168dd973
chang27896 <charles27896@gmail.com>
2021-04-26 16:02:36 +0800

    final

--- timer/script.js
+++ timer/script.js
@@ -1,3 +1,8 @@
+const daysE1 = document.getElementById('days')
+const hoursE1 = document.getElementById('hours')
+const minutesE1 = document.getElementById('mins')
+const secondsE1 = document.getElementById('seconds')
+
 const newyears = "1 Jan 2022";
 
 function countdown() {
@@ -10,7 +15,14 @@ function countdown() {
     const minutes = Math.floor(seconds / 60) % 60;
     const second = Math.floor(seconds % 60);
 
-    console.log(days, hours, minutes, second);
+    daysE1.innerHTML = days;
+    hoursE1.innerHTML = hours;
+    minutesE1.innerHTML = minutes;
+    secondsE1.innerHTML = formatTime(second);
+}
+
+function formatTime(time) {
+    return time < 10 ? (`0${time}`) : time;
 }
 
 countdown();
